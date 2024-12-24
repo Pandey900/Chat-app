36af1a94e8ea31eab246691022920d7c433f9b21
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-12-24 17:59:33 +0530

    2nd Part Completed UI design

---
+++ Frontend/src/components/messages/Messages.jsx
@@ -0,0 +1,53 @@
+import Message from "./Message";
+
+const Messages = () => {
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
+};
+
+export default Messages;
+
+//STARTED CODE FROM THE BEGINNING
+
+// import Message from "./Message";
+
+// const Messages = () => {
+//   return (
+//     <div className="px-4 flex-1 overflow-auto">
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//       <Message />
+//     </div>
+//   );
+// };
+
+// export default Messages;
---
+++ Frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,34 @@
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
+
+export default Conversations;
+
+// Started code for Conversations.jsx
+// import Conversation from "./Conversation";
+
+// const Conversations = () => {
+//   return (
+//     <div className="py-2 flex flex-col overflow-auto">
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//       <Conversation />
+//     </div>
+//   );
+// };
+
+// export default Conversations;
