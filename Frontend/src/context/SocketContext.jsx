33cedc18bae2875e72f179459dd701be5d921f1e
Abhishek Pandey <112680532+Pandey900@users.noreply.github.com>
2024-12-27 05:07:27 +0530

    Issue Resolved

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-yt.onrender.com", {
+      const socket = io("https://chat-app-abhi-hf8w.onrender.com/", {
         query: {
           userId: authUser._id,
         },
